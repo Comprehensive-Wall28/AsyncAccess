afc580cb449285744bc2e7ca8e22520484571057
Galileo204 <mg1022765@gmail.com>
2025-03-06 15:38:34 +0200

    "This is my second commit of the day and I am pissed about it >:("

---
+++ Software Engineering Files/Models/booking.js
@@ -0,0 +1,16 @@
+const bookingSchema = new mongoose.Schema({
+  user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+  },
+  event: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Event',
+      required: true,
+  },
+  numberOfTickets: {
+      type: Number,
+      required: true,
+    }
+  });
