5cdd8fa4f551190302abab0ec87ef31bbd0e4b2f
Youssef-Amrr <youssefamr.abdelbary@hotmail.com>
2025-04-10 18:19:39 +0200

    Added the Controllers and routes

---
+++ src/Server/routes/eventRoutes.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+    getAllEvents,
+    getEvent,
+    createEvent,
+    updateEvent,
+    deleteEvent
+} = require('../controllers/eventController');
+
+//Get User events
+//router.get('/user/:id', getUserEvents);  didn't do it yet
+
+//Get all events
+router.get('/events', getAllEvents);
+
+//Get one event
+router.get('/events/:id', getEvent);
+
+//Create event
+router.post('/events', createEvent);
+
+//Delete event
+router.delete('/events/:id', deleteEvent);
+
+//Update event
+router.patch('/events/:id', updateEvent);
+
+module.exports = router;
+
+
