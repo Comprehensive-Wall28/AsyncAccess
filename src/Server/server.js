f4f1fb8076d414c0f25a4065ea77099c805fc2e7
MostafaSolim <mostafasol@gmail.com>
2025-04-11 15:41:01 +0200

    .

--- src/Server/server.js
+++ src/Server/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const cookieParser=require('cookie-parser')
 const connectDB = require('./config/database');
 
 const port = process.env.PORT || 4001;
