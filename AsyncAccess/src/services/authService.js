3ba58213f77927e092a212bedf2543611430e296
Comprehensive-Wall28 <comprehensive.wall28@gmail.com>
2025-05-16 22:31:22 +0300

    Remove env and a minor fix

--- AsyncAccess/src/services/authService.js
+++ AsyncAccess/src/services/authService.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const API_BASE_URL ='http://localhost:5173/api/v1';
+const BACKEND_STATIC_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const apiClientInstance = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: BACKEND_STATIC_BASE_URL,
   withCredentials: true,
 });
 
