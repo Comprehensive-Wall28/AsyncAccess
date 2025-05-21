58dd860b93a48771e1c84238c704874c46c4c9b8
ShadowStrike85 <adamr662005@gmail.com>
2025-05-21 21:57:25 +0300

    added more styling to showing users

--- AsyncAccess/src/dashboard-admin/components/UsersList.jsx
+++ AsyncAccess/src/dashboard-admin/components/UsersList.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import {
-  Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper,
-  TablePagination, TextField, Box
-} from '@mui/material';
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import TablePagination from '@mui/material/TablePagination';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
 
 function UsersList({ users }) {
   const [search, setSearch] = React.useState('');
   const [page, setPage] = React.useState(0);
+
+  const usersPerPage = 10;
 
   const filteredUsers = users.filter(
     user =>
@@ -14,12 +22,11 @@ function UsersList({ users }) {
       user.email?.toLowerCase().includes(search.toLowerCase())
   );
 
-  const usersPerPage = 10;
   const paginatedUsers = filteredUsers.slice(page * usersPerPage, page * usersPerPage + usersPerPage);
 
   return (
-    <Paper sx={{ width: '100%', overflow: 'hidden', mt: 2 }}>
-      <Box sx={{ display: 'flex', justifyContent: 'flex-end', p: 2 }}>
+    <React.Fragment>
+      <Box sx={{ display: 'flex', justifyContent: 'flex-end', mb: 2 }}>
         <TextField
           label="Search users"
           variant="outlined"
@@ -31,8 +38,8 @@ function UsersList({ users }) {
           }}
         />
       </Box>
-      <TableContainer>
-        <Table>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="users table">
           <TableHead>
             <TableRow>
               <TableCell>Name</TableCell>
@@ -41,16 +48,23 @@ function UsersList({ users }) {
             </TableRow>
           </TableHead>
           <TableBody>
-            {paginatedUsers.map(user => (
-              <TableRow key={user.id || user._id}>
-                <TableCell>{user.name}</TableCell>
+            {paginatedUsers.map((user) => (
+              <TableRow
+                key={user.id || user._id}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {user.name}
+                </TableCell>
                 <TableCell>{user.email}</TableCell>
                 <TableCell>{user.role}</TableCell>
               </TableRow>
             ))}
             {paginatedUsers.length === 0 && (
               <TableRow>
-                <TableCell colSpan={3} align="center">No users found.</TableCell>
+                <TableCell colSpan={3} align="center">
+                  No users found.
+                </TableCell>
               </TableRow>
             )}
           </TableBody>
@@ -64,7 +78,7 @@ function UsersList({ users }) {
         rowsPerPage={usersPerPage}
         rowsPerPageOptions={[usersPerPage]}
       />
-    </Paper>
+    </React.Fragment>
   );
 }
 
