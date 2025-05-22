90c17b13cc161e0801e6ab095fc392aad65f5b7f
Youssef-Amrr <youssefamr.abdelbary@hotmail.com>
2025-05-22 23:40:16 +0300

    Fixed some issues

--- AsyncAccess/src/App.jsx
+++ AsyncAccess/src/App.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import MainLayout from './components/MainLayout';
 import Home from './home-page/Home';
 import SignIn from './sign-in/SignIn';
 import SignUp from './sign-up/SignUp';
 import Dashboard from './dashboard/Dashboard';
 
 import Events from './events/Events'
-import EventListing from './organizer-page/EventListing.jsx';
-import EventAnalytics from './organizer-page/Details/EventAnalytics.jsx';
+
 import BookingListing from "./booking-page/Checkout.jsx";
 import Unauthorized from './unauthorized/Unauthorized';
 import Unauthenticated from './unauthenticated/Unauthenticated';
@@ -19,7 +17,6 @@ import RoledSignup from './sign-up-role/SignUp.jsx'
 import ForgotPassword from './forgot-password/ForgotPassword.jsx';
 import DashboardAdmin from './dashboard-admin/Dashboard.jsx'
 
-import OrganizerDashboard from './organizer-page/EventListing.jsx';
 import EventsPage from './events/Events'; // Assuming 'Events.jsx' is the page wrapper
 import EventDetails from './events/components/EventDetails'; // Corrected import path
 
@@ -71,26 +68,18 @@ function App() {
                     element={<Events />}
                 />
 
-                <Route
-                    path="/dashboard"
-                    element={
-                        <MainLayout>
-                            {/* Replace HomePageContent with your actual dashboard component later */}
-                        </MainLayout>
-                    }
-                />
 
 
 
                 {/*<Route path="/organizer-management" element={<OrganizerDashboard />} />*/}
-                <Route path="/events" element={<EventListing />} />
+
                 <Route path="/bookings" element={<BookingListing />} />
                 {/*<Route path="events/:id" element={<EventAnalytics />} />*/}
                 <Route path="/login-roled" element={<RoledLogin />} />
                 <Route path="/signup-roled" element={<RoledSignup />} />
                 <Route path="/forgot-password" element={<ForgotPassword />} />
                 <Route path="/dashboard-admin" element={<DashboardAdmin />} />
-                <Route path="/organizer-management" element={<OrganizerDashboard />} />
+
 
                 <Route
                     path="/unauthorized"
